16f237f93dc97baf558889fdcd5f0a6d3b496d10
Xavier D'Mello <xaviermdmello@gmail.com>
2025-02-27 17:31:10 -0700

    working

--- src/FaceBuddy.sol
+++ src/FaceBuddy.sol
@@ -8,6 +8,7 @@ import { IV4Router } from "@uniswap/v4-periphery/src/interfaces/IV4Router.sol";
 import { Actions } from "@uniswap/v4-periphery/src/libraries/Actions.sol";
 import { IPermit2 } from "@uniswap/permit2/src/interfaces/IPermit2.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { StateLibrary } from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
 
 contract Example {
     using StateLibrary for IPoolManager;
@@ -16,7 +17,7 @@ contract Example {
     IPoolManager public immutable poolManager;
     IPermit2 public immutable permit2;
 
-    constructor(address _router, address _poolManager, address _permit2) {
+    constructor(address payable _router, address _poolManager, address _permit2) {
         router = UniversalRouter(_router);
         poolManager = IPoolManager(_poolManager);
         permit2 = IPermit2(_permit2);
